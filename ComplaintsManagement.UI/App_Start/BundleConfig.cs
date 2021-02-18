5f5fd9ccd89da2cf46d8dc0b44362da8429d08c0
JhonasV <jhonas@gmail.com>
2021-02-18 01:45:16 -0400

    boostrap update to v4, material UI theme added.

--- ComplaintsManagement.UI/App_Start/BundleConfig.cs
+++ ComplaintsManagement.UI/App_Start/BundleConfig.cs
@@ -14,17 +14,26 @@ namespace ComplaintsManagement.UI
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+
+            bundles.Add(new ScriptBundle("~/bundles/jquerydatatable").Include(
+                        "~/Scripts/DataTables/jquery.dataTables.js",
+                        "~/Scripts/DataTables/dataTables.bootstrap4.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(         
+                "~/Scripts/site.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/DataTables/css/dataTables.bootstrap4.css"));
         }
     }
 }
